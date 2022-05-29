08449a1236a8774fb97995c8108db76f97659a4a
SruthiGomathi <86158320+SruthiGomathi@users.noreply.github.com>
2022-05-29 22:23:02 +0530

    Prepare>Algorithms>Warmup>Mini Max Sum
    https://www.hackerrank.com/challenges/mini-max-sum/problem
    
    Given five positive integers, find the minimum and maximum values that can be calculated by summing exactly four of the five integers. Then print the respective minimum and maximum values as a single line of two space-separated long integers.
    
    Example
    arr = [1.3.5.7.9]
    The minimum sum is 1+3+5+7 = 16 and the maximum sum is 3+5+7+9 = 24. The function prints
    16 24
    
    Function Description
    Complete the miniMaxSum function in the editor below.
    miniMaxSum has the following parameter(s):
    arr: an array of 5 integers
    
    Print
    Print two space-separated integers on one line: the minimum sum and the maximum sum of 4 of 5 elements.
    
    Input Format
    A single line of five space-separated integers.
    
    Constraints
    1 <= arr[i] <= 10^9
    
    Output Format
    Print two space-separated long integers denoting the respective minimum and maximum values that can be calculated by summing exactly four of the five integers. (The output can be greater than a 32 bit integer.)
    
    Sample Input
    1 2 3 4 5
    
    Sample Output
    10 14
    
    Explanation
    The numbers are 1, 2, 3, 4, and 5. Calculate the following sums using four of the five integers:
    
    Sum everything except 1, the sum is 2+3+4+5=14.
    Sum everything except 2, the sum is 1+3+4+5=13.
    Sum everything except 3, the sum is 1+2+4+5=12.
    Sum everything except 4, the sum is 1+2+3+5=11.
    Sum everything except 5, the sum is 1+2+3+4=10.
    
    Hints: Beware of integer overflow! Use 64-bit Integer.
    Need help to get started? Try the Solve Me First problem

---
+++ miniMaxSum.java
@@ -0,0 +1,98 @@
+/* https://www.hackerrank.com/challenges/mini-max-sum/problem
+
+Given five positive integers, find the minimum and maximum values that can be calculated by summing exactly four of the five integers. Then print the respective minimum and maximum values as a single line of two space-separated long integers.
+
+Example
+arr = [1.3.5.7.9]
+The minimum sum is 1+3+5+7 = 16 and the maximum sum is 3+5+7+9 = 24. The function prints
+16 24
+
+Function Description
+Complete the miniMaxSum function in the editor below.
+miniMaxSum has the following parameter(s):
+arr: an array of 5 integers
+
+Print
+Print two space-separated integers on one line: the minimum sum and the maximum sum of 4 of 5 elements.
+
+Input Format
+A single line of five space-separated integers.
+
+Constraints
+1 <= arr[i] <= 10^9
+
+Output Format
+Print two space-separated long integers denoting the respective minimum and maximum values that can be calculated by summing exactly four of the five integers. (The output can be greater than a 32 bit integer.)
+
+Sample Input
+1 2 3 4 5
+
+Sample Output
+10 14
+
+Explanation
+The numbers are 1, 2, 3, 4, and 5. Calculate the following sums using four of the five integers:
+
+Sum everything except 1, the sum is 2+3+4+5=14.
+Sum everything except 2, the sum is 1+3+4+5=13.
+Sum everything except 3, the sum is 1+2+4+5=12.
+Sum everything except 4, the sum is 1+2+3+5=11.
+Sum everything except 5, the sum is 1+2+3+4=10.
+
+Hints: Beware of integer overflow! Use 64-bit Integer.
+Need help to get started? Try the Solve Me First problem */
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+class Result {
+
+    /*
+     * Complete the 'miniMaxSum' function below.
+     *
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+//-------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+    public static void miniMaxSum(List<Integer> arr) {
+    // Write your code here
+        int n = arr.size();
+        long array[] = new long[n];
+        long maxValue = 0, minValue = 9223372036854775807l;
+        for(int i=0;i<arr.size();i++){
+            for(int j=0;j<arr.size();j++){
+                if(i!=j)
+                    array[i] += arr.get(j);
+            }
+            if(array[i]>maxValue)
+                maxValue = array[i];
+            if(array[i]<minValue)
+                minValue = array[i];
+        }
+        System.out.println(minValue +" " + maxValue);
+    }
+//-------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] arrTemp = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        List<Integer> arr = new ArrayList<>();
+
+        for (int i = 0; i < 5; i++) {
+            int arrItem = Integer.parseInt(arrTemp[i]);
+            arr.add(arrItem);
+        }
+
+        Result.miniMaxSum(arr);
+
+        bufferedReader.close();
+    }
+}
