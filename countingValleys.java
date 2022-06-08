16d725d693b7f5a0ff30d1d2ebde512bb986e638
Sruthi Gomathi <86158320+SruthiGomathi@users.noreply.github.com>
2022-06-08 12:57:33 +0530

    Prepare>Algorithms>Implementation>Counting Valleys
    https://www.hackerrank.com/challenges/counting-valleys/problem
    
    An avid hiker keeps meticulous records of their hikes. During the last hike that took exactly  steps, for every step it was noted if it was an uphill, U, or a downhill, D step. Hikes always start and end at sea level, and each step up or down represents a 1 unit change in altitude. We define the following terms:
    
    - A mountain is a sequence of consecutive steps above sea level, starting with a step up from sea level and ending with a step down to sea level.
    - A valley is a sequence of consecutive steps below sea level, starting with a step down from sea level and ending with a step up to sea level.
    Given the sequence of up and down steps during a hike, find and print the number of valleys walked through.
    
    Example
    steps = 8 path =[DDUUUUDD]
    
    The hiker first enters a valley 2 units deep. Then they climb out and up onto a mountain 2 units high. Finally, the hiker returns to sea level and ends the hike.
    
    Function Description
    Complete the countingValleys function in the editor below.
    countingValleys has the following parameter(s):
    int steps: the number of steps on the hike
    string path: a string describing the path
    
    Returns
    int: the number of valleys traversed
    
    Input Format
    The first line contains an integer steps, the number of steps in the hike.
    The second line contains a single string path, of steps characters that describe the path.
    
    Constraints
    2 <= steps <= 10^6
    path [i] element of {UD}
    
    Sample Input
    8
    UDDDUDUU
    
    Sample Output
    1
    
    Explanation
    If we represent _ as sea level, a step up as /, and a step down as \, the hike can be drawn as:
    _/\         _
        \      /
          \/\/
    The hiker enters and leaves one valley.

---
+++ countingValleys.java
@@ -0,0 +1,99 @@
+/* https://www.hackerrank.com/challenges/counting-valleys/problem
+
+An avid hiker keeps meticulous records of their hikes. During the last hike that took exactly  steps, for every step it was noted if it was an uphill, U, or a downhill, D step. Hikes always start and end at sea level, and each step up or down represents a 1 unit change in altitude. We define the following terms:
+
+- A mountain is a sequence of consecutive steps above sea level, starting with a step up from sea level and ending with a step down to sea level.
+- A valley is a sequence of consecutive steps below sea level, starting with a step down from sea level and ending with a step up to sea level.
+Given the sequence of up and down steps during a hike, find and print the number of valleys walked through.
+
+Example
+steps = 8 path =[DDUUUUDD]
+
+The hiker first enters a valley 2 units deep. Then they climb out and up onto a mountain 2 units high. Finally, the hiker returns to sea level and ends the hike.
+
+Function Description
+Complete the countingValleys function in the editor below.
+countingValleys has the following parameter(s):
+int steps: the number of steps on the hike
+string path: a string describing the path
+
+Returns
+int: the number of valleys traversed
+
+Input Format
+The first line contains an integer steps, the number of steps in the hike.
+The second line contains a single string path, of steps characters that describe the path.
+
+Constraints
+2 <= steps <= 10^6
+path [i] element of {UD}
+
+Sample Input
+8
+UDDDUDUU
+
+Sample Output
+1
+
+Explanation
+If we represent _ as sea level, a step up as /, and a step down as \, the hike can be drawn as:
+_/\         _
+    \      /
+      \/\/
+The hiker enters and leaves one valley. */
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+class Result {
+
+    /*
+     * Complete the 'countingValleys' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER steps
+     *  2. STRING path
+     */
+//------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+    public static int countingValleys(int steps, String path) {
+    // Write your code here
+        int valleyCount=0;
+        int fromSeaLevel = 0;
+        for(int i=0;i<steps;i++){
+            if(path.charAt(i)=='D' && fromSeaLevel == 0)
+                ++valleyCount;
+            if(path.charAt(i) == 'D'){
+                --fromSeaLevel;
+            }  
+            else
+                ++fromSeaLevel;
+        }
+        return valleyCount;
+    }
+//------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int steps = Integer.parseInt(bufferedReader.readLine().trim());
+
+        String path = bufferedReader.readLine();
+
+        int result = Result.countingValleys(steps, path);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
